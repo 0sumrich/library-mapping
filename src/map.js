ff7f4cf41fc48624a1f19af1c84cbd102a35427f
Richard Lynch <lynchardrich@gmail.com>
2020-01-12 10:43:56 +0000

    tidied

--- src/map.js
+++ src/map.js
@@ -1,47 +1,14 @@
-import React, { Fragment, useState, useEffect } from "react";
-import ReactMapboxGl, { Layer, GeoJSONLayer } from "react-mapbox-gl";
+import React, { useState } from "react";
+import ReactMapboxGl from "react-mapbox-gl";
 import Barnet from "./barnet";
-import * as d3 from "d3";
 
 const publickey = process.env.REACT_APP_API_KEY;
+
 const MapBox = ReactMapboxGl({
 	accessToken: publickey
 });
 
 const style = "mapbox://styles/0sumrich/ck55oh8go05ks1cqqi7lavhcq";
-
-const Layers = ({ features }) => {
-	const fPaint = {
-		"fill-color": "#ff00e1",
-		"fill-opacity": 0.25
-	};
-	const borrowers = features.map(o => o.properties["Number of borrowers"]);
-	const min = d3.min(borrowers);
-	const max = d3.max(borrowers);
-	const scale = d3
-		.scaleLinear()
-		.domain([min, max])
-		.range([0, 1]);
-	console.log(scale(152));
-	return features ? (
-		<Fragment>
-			{features.map((feature, i) => (
-				<GeoJSONLayer key={i} data={feature} fillPaint={fPaint} />
-			))}
-		</Fragment>
-	) : null;
-};
-
-//
-
-// useEffect(() => {
-// 	const fetchData = async () => {
-// 		const res = await fetch("/boundaries_with_id.json");
-// 		const json = await res.json();
-// 		setFeatures(json.features);
-// 	};
-// 	fetchData();
-// }, []);
 
 export default ({ la, mapStyle }) => {
 	let hoverId = null;
