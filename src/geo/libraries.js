b8df80aba6b055bdd00d60dc380b78f5926c2483
Richard Lynch <lynchardrich@gmail.com>
2020-01-22 20:00:50 +0000

    colindale location update

--- src/geo/libraries.js
+++ src/geo/libraries.js
@@ -1,1 +1,105 @@
-export const libraries = {"type":"FeatureCollection","features":[{"type":"Feature","geometry":{"type":"Point","coordinates":[-0.229028,51.588619]},"properties":{"library":"Hendon Library"}},{"type":"Feature","geometry":{"type":"Point","coordinates":[-0.16759,51.593929]},"properties":{"library":"East Finchley Library"}},{"type":"Feature","geometry":{"type":"Point","coordinates":[-0.263219871,51.603204078]},"properties":{"library":"Burnt Oak Library"}},{"type":"Feature","geometry":{"type":"Point","coordinates":[-0.204752954,51.65607394]},"properties":{"library":"Chipping Barnet Library"}},{"type":"Feature","geometry":{"type":"Point","coordinates":[-0.161600463,51.6444384]},"properties":{"library":"East Barnet Library"}},{"type":"Feature","geometry":{"type":"Point","coordinates":[-0.273996976,51.616294758]},"properties":{"library":"Edgware Library"}},{"type":"Feature","geometry":{"type":"Point","coordinates":[-0.201727357,51.57479125]},"properties":{"library":"Golders Green Library"}},{"type":"Feature","geometry":{"type":"Point","coordinates":[-0.244894575,51.601794883]},"properties":{"library":"Colindale Library"}},{"type":"Feature","geometry":{"type":"Point","coordinates":[-0.24299973,51.615344847]},"properties":{"library":"Mill Hill Library"}},{"type":"Feature","geometry":{"type":"Point","coordinates":[-0.175998832,51.618618547]},"properties":{"library":"North Finchley Library"}},{"type":"Feature","geometry":{"type":"Point","coordinates":[-0.149067988,51.630377142]},"properties":{"library":"Osidge Library"}},{"type":"Feature","geometry":{"type":"Point","coordinates":[-0.148018992,51.600463711]},"properties":{"library":"South Friern Library"}},{"type":"Feature","geometry":{"type":"Point","coordinates":[-0.19516,51.59992]},"properties":{"library":"Finchley Church End Library"}},{"type":"Feature","geometry":{"type":"Point","coordinates":[-0.20078133,51.5622378]},"properties":{"library":"Childs Hill Library"}}]}
+export const libraries = {
+	type: "FeatureCollection",
+	features: [
+		{
+			type: "Feature",
+			geometry: { type: "Point", coordinates: [-0.229028, 51.588619] },
+			properties: { library: "Hendon Library" }
+		},
+		{
+			type: "Feature",
+			geometry: { type: "Point", coordinates: [-0.16759, 51.593929] },
+			properties: { library: "East Finchley Library" }
+		},
+		{
+			type: "Feature",
+			geometry: {
+				type: "Point",
+				coordinates: [-0.263219871, 51.603204078]
+			},
+			properties: { library: "Burnt Oak Library" }
+		},
+		{
+			type: "Feature",
+			geometry: {
+				type: "Point",
+				coordinates: [-0.204752954, 51.65607394]
+			},
+			properties: { library: "Chipping Barnet Library" }
+		},
+		{
+			type: "Feature",
+			geometry: {
+				type: "Point",
+				coordinates: [-0.161600463, 51.6444384]
+			},
+			properties: { library: "East Barnet Library" }
+		},
+		{
+			type: "Feature",
+			geometry: {
+				type: "Point",
+				coordinates: [-0.273996976, 51.616294758]
+			},
+			properties: { library: "Edgware Library" }
+		},
+		{
+			type: "Feature",
+			geometry: {
+				type: "Point",
+				coordinates: [-0.201727357, 51.57479125]
+			},
+			properties: { library: "Golders Green Library" }
+		},
+		{
+			type: "Feature",
+			geometry: {
+				type: "Point",
+				coordinates: [-0.245778, 51.597438]
+			},
+			properties: { library: "Colindale Library" }
+		},
+		{
+			type: "Feature",
+			geometry: {
+				type: "Point",
+				coordinates: [-0.24299973, 51.615344847]
+			},
+			properties: { library: "Mill Hill Library" }
+		},
+		{
+			type: "Feature",
+			geometry: {
+				type: "Point",
+				coordinates: [-0.175998832, 51.618618547]
+			},
+			properties: { library: "North Finchley Library" }
+		},
+		{
+			type: "Feature",
+			geometry: {
+				type: "Point",
+				coordinates: [-0.149067988, 51.630377142]
+			},
+			properties: { library: "Osidge Library" }
+		},
+		{
+			type: "Feature",
+			geometry: {
+				type: "Point",
+				coordinates: [-0.148018992, 51.600463711]
+			},
+			properties: { library: "South Friern Library" }
+		},
+		{
+			type: "Feature",
+			geometry: { type: "Point", coordinates: [-0.19516, 51.59992] },
+			properties: { library: "Finchley Church End Library" }
+		},
+		{
+			type: "Feature",
+			geometry: { type: "Point", coordinates: [-0.20078133, 51.5622378] },
+			properties: { library: "Childs Hill Library" }
+		}
+	]
+};
