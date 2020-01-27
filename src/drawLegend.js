e25c71e52d6c66def3397d17a45b5a8c678a7c41
Richard Lynch <lynchardrich@gmail.com>
2020-01-27 20:08:08 +0000

    legend done

--- src/drawLegend.js
+++ src/drawLegend.js
@@ -11,42 +11,77 @@ import * as d3 from "d3";
 //       .append("g")
 //       .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-function drawLegend(colors) {
-	const legend = d3
-		.select("#legend")
-		.append("svg")
-		.attr("width", "90%")
-		.attr("height", "50px");
+function drawLegend(colors, numbers) {
+	
+	const legend = d3.select("#legend");
+	
+	const margin = { top: 5, right: 5, bottom: 5, left: 5 };
 
-	const { height, width } = legend.node().getBoundingClientRect();
+	const width =
+		legend.node().getBoundingClientRect().width -
+		margin.left -
+		margin.right;
+
+	const height = 50 - margin.top - margin.bottom;
+	
+	const g = legend
+		.append("svg")
+		.attr("width", width)
+		.attr("height", height)
+		.append("g")
+		.attr("transform", `translate(${margin.left}, ${margin.top})`);
+
 	const scaleLeft = d3
 		.scaleLinear()
 		.domain([0, width / 2])
 		.range([0, 1]);
+	
 	const colorLeft = px =>
 		d3.interpolateRgb(colors[0], colors[1])(scaleLeft(px));
+	
 	const scaleRight = d3
 		.scaleLinear()
 		.domain([width / 2, width])
 		.range([0, 1]);
+	
 	const colorRight = px =>
 		d3.interpolateRgb(colors[1], colors[2])(scaleRight(px));
+	
 	for (let i = 0; i < width; i++) {
 		const attrs = {
-				x: i,
-				y: 0,
-				width: 1,
-				height: height,
-				fill: i < width / 2 ? colorLeft(i) : colorRight(i)
-			};
-		legend
-			.append("rect")
-			.attr('x', attrs.x)
-			.attr('y', attrs.y)
-			.attr('width', attrs.width)
-			.attr('height', attrs.height)
-			.attr('fill', attrs.fill)
+			x: i,
+			y: 0,
+			width: 1,
+			height: height / 2,
+			fill: i < width / 2 ? colorLeft(i) : colorRight(i)
+		};
+		g.append("rect")
+			.attr("x", attrs.x)
+			.attr("y", attrs.y)
+			.attr("width", attrs.width)
+			.attr("height", attrs.height)
+			.attr("fill", attrs.fill);
 	}
+	
+	g.append("text")
+		.attr("x", 0)
+		.attr("y", 10 + height / 2)
+		.attr("class", "legend-text")
+		.text(numbers[0]);
+
+	g.append("text")
+		.attr("x", width / 2)
+		.attr("y", 10 + height / 2)
+		.attr("class", "legend-text")
+		.attr('text-anchor', 'middle')
+		.text(numbers[1]);
+
+	g.append("text")
+		.attr("x", width - 5)
+		.attr("y", 10 + height / 2)
+		.attr("class", "legend-text")
+		.attr("text-anchor", "end")
+		.text(numbers[2]);
 }
 
 export default drawLegend;
--- src/legend.js
+++ src/legend.js
@@ -1,30 +1,43 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import drawLegend from "./drawLegend";
 
-const styleId = 'ck55oh8go05ks1cqqi7lavhcq';
+const styleId = "ck55oh8go05ks1cqqi7lavhcq";
 const publickey = process.env.REACT_APP_API_KEY;
-const url = `https://api.mapbox.com/styles/v1/0sumrich/${styleId}?access_token=${publickey}`
+const url = `https://api.mapbox.com/styles/v1/0sumrich/${styleId}?access_token=${publickey}`;
 
 const Legend = () => {
+	const [hidden, setHidden] = useState(false);
 	useEffect(() => {
 		const fetchData = async () => {
 			try {
 				const res = await fetch(url);
 				const json = await res.json();
 				const paint = json.layers[73].paint["fill-color"];
-				const colors = [paint[4], paint[6], paint[8]]
-				drawLegend(colors);
+				const colors = [paint[4], paint[6], paint[8]];
+				const numbers = [paint[3], paint[5], paint[7]];
+				drawLegend(colors, numbers);
 			} catch (err) {
 				console.log(err);
 			}
 		};
 		fetchData();
 	}, []);
+
+	const spanClick = () => setHidden(!hidden);
+	// const spanClick = () => console.log("hi");
 	return (
-		<div id="legend">
+		<div id="legend" className={hidden ? "hidden" : null}>
 			<p>Number of members in each area</p>
+			<div className="span" onClick={spanClick}>
+				<img
+					className="icon"
+					src="close.png"
+					height="10px"
+					width="10px"
+				/>
+			</div>
 		</div>
-		);
+	);
 };
 
 export default Legend;
