9ca779d1a0d33a2757f801ccf410a2e18f24e30d
Richard Lynch <lynchardrich@gmail.com>
2020-01-28 19:26:22 +0000

    Updates

---
+++ precache-manifest.3e7889a0b75caa52347e0ba0c911fb08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c01cdfdd3bdf9449eb39f1e839d01c7",
+    "url": "/library-mapping/index.html"
+  },
+  {
+    "revision": "27e9c02005b77b931c56",
+    "url": "/library-mapping/static/css/2.7efaf445.chunk.css"
+  },
+  {
+    "revision": "323ef907c1e6db5c8bea",
+    "url": "/library-mapping/static/css/main.a24079bd.chunk.css"
+  },
+  {
+    "revision": "27e9c02005b77b931c56",
+    "url": "/library-mapping/static/js/2.84d076b4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/library-mapping/static/js/2.84d076b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "323ef907c1e6db5c8bea",
+    "url": "/library-mapping/static/js/main.8e4852ab.chunk.js"
+  },
+  {
+    "revision": "cc1d5df4a39efea17a8f",
+    "url": "/library-mapping/static/js/runtime-main.aaf1b0e4.js"
+  }
+]);
